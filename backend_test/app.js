d4d0916ed342d6e9e24f07bb2e88e929325568f8
Shio-Ak <salt.strawhat.5656@gmail.com>
2024-11-22 00:14:54 +0900

    微修正

--- backend_test/app.js
+++ backend_test/app.js
@@ -2,6 +2,6 @@ const express = require("express");
 const app = express();
 const anyRoutes = require("./routes/anyRoutes");
 
-app.use("/requests/*", anyRoutes);
+app.use("/requests", anyRoutes);
 
 module.exports = app;
