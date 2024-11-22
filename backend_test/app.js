6172f4871763d7c381e5a2e80528238536afec5f
Shio-Ak <salt.strawhat.5656@gmail.com>
2024-11-22 18:15:17 +0900

    lambdaを利用する部分以外の環境を合わせる

--- backend_test/app.js
+++ backend_test/app.js
@@ -1,6 +1,15 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const anyRoutes = require("./routes/anyRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 
 app.use("/requests", anyRoutes);
 
