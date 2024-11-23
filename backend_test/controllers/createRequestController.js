cb876ae29ed5c61d4119fd03b1ed202e3b8ce605
Shio-Ak <salt.strawhat.5656@gmail.com>
2024-11-24 00:06:04 +0900

    mockの状態を解除

--- backend_test/controllers/createRequestController.js
+++ backend_test/controllers/createRequestController.js
@@ -20,20 +20,14 @@ exports.getAllGratitudes = async (req, res) => {
 };
 
 exports.postRequest = async (req, res) => {
-  const mockObj = {
-    userId: 1,
-    gratitudeId: 3,
-    requesterComment: "nice to meet you!",
-    totalMaxPrice: 500,
-    itemIds: [1, 2],
-    responderId: 2,
-  };
+  reqBodyObj = req.body;
+
   try {
     const menuResult = await createRequestModel.postMenu({
-      total_max_price: mockObj.totalMaxPrice,
+      total_max_price: reqBodyObj.totalMaxPrice,
     });
 
-    const menuDetailArray = mockObj.itemIds.map((value) => {
+    const menuDetailArray = reqBodyObj.itemIds.map((value) => {
       return { menu_id: menuResult[0].menu_id, item_id: value };
     });
 
@@ -44,21 +38,21 @@ exports.postRequest = async (req, res) => {
     );
 
     const requestResult = await createRequestModel.postRequest({
-      user_id: mockObj.userId,
+      user_id: reqBodyObj.userId,
       menu_id: menuResult[0].menu_id,
-      gratitude_id: mockObj.gratitudeId,
-      requester_comment: mockObj.requesterComment,
+      gratitude_id: reqBodyObj.gratitudeId,
+      requester_comment: reqBodyObj.requesterComment,
     });
 
     await createRequestModel.postResponder({
       request_id: requestResult[0].request_id,
-      user_id: mockObj.responderId,
+      user_id: reqBodyObj.responderId,
     });
 
     await listRequestsModel.postStatus({
       request_id: requestResult[0].request_id,
       status_id: 1,
-      user_id: mockObj.userId,
+      user_id: reqBodyObj.userId,
     });
 
     res.status(200).end();
