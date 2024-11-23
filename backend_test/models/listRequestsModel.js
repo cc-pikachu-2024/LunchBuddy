73a0bd5ee7392e1aaa88b88b72b5f06f62aeb2e7
齋藤達也 <tatsuya@saitoutatsuyanoMacBook-Air.local>
2024-11-23 10:44:29 +0900

    getAllrequests実装（仮）

--- backend_test/models/listRequestsModel.js
+++ backend_test/models/listRequestsModel.js
@@ -2,14 +2,35 @@ const knex = require('../db/knex');
 
 module.exports = {
   async getAllRequests() {
-    return knex.select('*').from('request');
+    const latestStatusIds = await knex('request_status_history')
+      .select('request_history_id')
+      .whereRaw(
+        'created_at = (SELECT MAX(created_at) FROM request_status_history AS rh2 WHERE rh2.request_id = request_status_history.request_id)'
+      );
+
+    return knex('request')
+      .join('menu', 'request.menu_id', 'menu.menu_id')
+      .join('menu_detail', 'menu.menu_id', 'menu_detail.menu_id')
+      .join('item_master', 'menu_detail.item_id', 'item_master.item_id')
+      .join('gratitude', 'request.gratitude_id', 'gratitude.gratitude_id')
+      .join(
+        'request_status_history',
+        'request.request_id',
+        'request_status_history.request_id'
+      )
+      .leftjoin('responder', 'request.request_id', 'responder.request_id')
+      .whereIn('request_status_history.request_history_id', latestStatusIds)
+      .select('*');
   },
+  //INFO: 各リクエストごとにmenu_detailの数だけレコードができる。
+  //      item_nameをまとめて出すためにはこうするしかないか？
+  //      この場合、controllerでいい感じにまとめて返してあげる必要があるか。
+  //TODO: とりあえずselect('*')にしてしまっているが、列を絞る必要があるか要検討。
 };
 
 module.exports = {
   async getGratitudesPriceSum(user_id) {
-    return knex
-      .select('purchase')
+    return knex('purchase')
       .join(
         'purchase_detail',
         'purchase.purchase_id',
