8e63940981fa15b09459477cb74a93f28042b4a0
Shiba-You <s.y.chocola2921@gmail.com>
2024-11-25 02:02:54 +0900

    ルートをSignupに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./styles/App.css";
-import Login from "./pages/login";
+// import Login from "./pages/login";
 import Signup from "./pages/signup";
 import RequestList from "./pages/requestList";
 import RequestDetail from "./pages/requestDetail";
@@ -11,7 +11,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* // TODO: ルートパスをLoginへ変更*/}
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Signup />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/requestList" element={<RequestList />} />
         <Route path="/requestDetail" element={<RequestDetail />} />
