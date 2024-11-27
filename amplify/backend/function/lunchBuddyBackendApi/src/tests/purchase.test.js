b9826c1536277f8345d661286a35055bddf68f56
Shiba-You <s.y.chocola2921@gmail.com>
2024-11-27 15:18:27 +0900

    機能実装

--- amplify/backend/function/lunchBuddyBackendApi/src/tests/purchase.test.js
+++ amplify/backend/function/lunchBuddyBackendApi/src/tests/purchase.test.js
@@ -38,123 +38,125 @@ describe("Purchase API", async () => {
     sandbox.restore();
   });
 
-  it("商品購入して明細を送信するとレスポンスとして、purchase_detail_idを返却する（ステータス:200）", async () => {
-    const reqBody = {
-      requestId: 1,
-      responderId: 2,
-      recieptId: "",
-      itemList: [
-        {
-          itemName: "おにぎり",
-          inputPrice: 100,
-          menuFlag: true,
-        },
-        {
-          itemName: "いいおにぎり",
-          inputPrice: 150,
-          menuFlag: true,
-        },
-        {
-          itemName: "コーヒー",
-          inputPrice: 150,
-          menuFlag: true,
-        },
-        {
-          itemName: "お菓子",
-          inputPrice: 200,
-          menuFlag: false,
-        },
-      ],
-    };
-    const res = await request.post("/requests/purchase").send(reqBody);
+  describe("post purchase", () => {
+    it("商品購入して明細を送信するとレスポンスとして、purchase_detail_idを返却する（ステータス:200）", async () => {
+      const reqBody = {
+        requestId: 1,
+        responderId: 2,
+        recieptId: "",
+        itemList: [
+          {
+            itemName: "おにぎり",
+            inputPrice: 100,
+            menuFlag: true,
+          },
+          {
+            itemName: "いいおにぎり",
+            inputPrice: 150,
+            menuFlag: true,
+          },
+          {
+            itemName: "コーヒー",
+            inputPrice: 150,
+            menuFlag: true,
+          },
+          {
+            itemName: "お菓子",
+            inputPrice: 200,
+            menuFlag: false,
+          },
+        ],
+      };
+      const res = await request.post("/requests/purchase").send(reqBody);
 
-    expect(res).to.have.status(200);
-    expect(res.body).to.be.an("object");
+      expect(res).to.have.status(200);
+      expect(res.body).to.be.an("object");
 
-    const dbPurchase = await knex("purchase")
-      .select("*")
-      .where("purchase_id", 3)
-      .first();
-    const expectDbPurchase = {
-      purchase_id: 3,
-      request_id: 1,
-      responder_id: 2,
-      reciept_id: "",
-    };
-    expect(dbPurchase).to.deep.equal(expectDbPurchase);
-
-    const dbPurchaseDetail = await knex("purchase_detail")
-      .select("*")
-      .where("purchase_id", dbPurchase.purchase_id);
-    const expectDbPurchaseDetail = [
-      {
-        purchase_detail_id: 9,
+      const dbPurchase = await knex("purchase")
+        .select("*")
+        .where("purchase_id", 3)
+        .first();
+      const expectDbPurchase = {
         purchase_id: 3,
-        item_name: "おにぎり",
-        input_price: 100,
-        menu_flag: true,
-      },
-      {
-        purchase_detail_id: 10,
-        purchase_id: 3,
-        item_name: "いいおにぎり",
-        input_price: 150,
-        menu_flag: true,
-      },
-      {
-        purchase_detail_id: 11,
-        purchase_id: 3,
-        item_name: "コーヒー",
-        input_price: 150,
-        menu_flag: true,
-      },
-      {
-        purchase_detail_id: 12,
-        purchase_id: 3,
-        item_name: "お菓子",
-        input_price: 200,
-        menu_flag: false,
-      },
-    ];
-    expect(dbPurchaseDetail).to.deep.equal(expectDbPurchaseDetail);
-  });
+        request_id: 1,
+        responder_id: 2,
+        reciept_id: "",
+      };
+      expect(dbPurchase).to.deep.equal(expectDbPurchase);
 
-  it("商品購入時に通信エラーが起きた時、Errorをを返す（ステータス:500）", async () => {
-    sandbox
-      .stub(purchaseModel, "putPurchase")
-      .rejects(new Error("Database Error"));
-
-    const reqBody = {
-      requestId: 1,
-      responderId: 2,
-      recieptId: "",
-      itemList: [
+      const dbPurchaseDetail = await knex("purchase_detail")
+        .select("*")
+        .where("purchase_id", dbPurchase.purchase_id);
+      const expectDbPurchaseDetail = [
         {
-          itemName: "おにぎり",
-          inputPrice: 100,
-          menuFlag: true,
+          purchase_detail_id: 9,
+          purchase_id: 3,
+          item_name: "おにぎり",
+          input_price: 100,
+          menu_flag: true,
         },
         {
-          itemName: "いいおにぎり",
-          inputPrice: 150,
-          menuFlag: true,
+          purchase_detail_id: 10,
+          purchase_id: 3,
+          item_name: "いいおにぎり",
+          input_price: 150,
+          menu_flag: true,
         },
         {
-          itemName: "コーヒー",
-          inputPrice: 150,
-          menuFlag: true,
+          purchase_detail_id: 11,
+          purchase_id: 3,
+          item_name: "コーヒー",
+          input_price: 150,
+          menu_flag: true,
         },
         {
-          itemName: "お菓子",
-          inputPrice: 200,
-          menuFlag: false,
+          purchase_detail_id: 12,
+          purchase_id: 3,
+          item_name: "お菓子",
+          input_price: 200,
+          menu_flag: false,
         },
-      ],
-    };
+      ];
+      expect(dbPurchaseDetail).to.deep.equal(expectDbPurchaseDetail);
+    });
 
-    const res = await request.post("/requests/purchase").send(reqBody);
+    it("商品購入時に通信エラーが起きた時、Errorをを返す（ステータス:500）", async () => {
+      sandbox
+        .stub(purchaseModel, "postPurchase")
+        .rejects(new Error("Database Error"));
 
-    expect(res).to.have.status(500);
-    expect(res.body).to.be.an("object");
+      const reqBody = {
+        requestId: 1,
+        responderId: 2,
+        recieptId: "",
+        itemList: [
+          {
+            itemName: "おにぎり",
+            inputPrice: 100,
+            menuFlag: true,
+          },
+          {
+            itemName: "いいおにぎり",
+            inputPrice: 150,
+            menuFlag: true,
+          },
+          {
+            itemName: "コーヒー",
+            inputPrice: 150,
+            menuFlag: true,
+          },
+          {
+            itemName: "お菓子",
+            inputPrice: 200,
+            menuFlag: false,
+          },
+        ],
+      };
+
+      const res = await request.post("/requests/purchase").send(reqBody);
+
+      expect(res).to.have.status(500);
+      expect(res.body).to.be.an("object");
+    });
   });
 });
