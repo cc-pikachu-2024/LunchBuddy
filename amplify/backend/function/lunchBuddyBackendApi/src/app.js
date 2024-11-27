e80582070d0c160d4b115f33399ad1a7e6e221be
Shiba-You <s.y.chocola2921@gmail.com>
2024-11-27 22:32:16 +0900

    DBの修正

--- amplify/backend/function/lunchBuddyBackendApi/src/app.js
+++ amplify/backend/function/lunchBuddyBackendApi/src/app.js
@@ -20,7 +20,7 @@ app.use("/requests", routes);
 
 // ローカル環境用のサーバー起動コード
 if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 3000;
+  const PORT = process.env.DB_PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
   });
